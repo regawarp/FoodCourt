14096af6d2978dccd2b316859c3874850b3b7cd0
Regawa <regawa04@gmail.com>
2018-06-02 09:30:00 +0700

    Edit Form Toko dan Integrasi: - Tampil gambar

--- src/foodcourt/Toko.java
+++ src/foodcourt/Toko.java
@@ -9,13 +9,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
