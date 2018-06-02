9b5659490ea032fd5312e79202c630cc7a080bf9
Atika Pane <atikapane17@gmail.com>
2018-06-02 10:34:58 +0700

    Edit dashboard toko

--- src/foodcourt/DashboardToko.java
+++ src/foodcourt/DashboardToko.java
@@ -138,7 +138,7 @@ public class DashboardToko extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        new TakePesananDariMeja().setVisible(true);
+        new TakeOrder().setVisible(true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
